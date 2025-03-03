ce6e878b2a261906923acae4337699ebe00be1c1
Aidan Stoaks <aidanstoaks05@gmail.com>
2025-03-03 16:33:51 +0100

    Added operate function to perform calculations

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid operator!";
+    }
+}
